ca57f105c9209b9fbccb91b4470bab7dd0cdb46c
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2024-09-02 16:30:38 +0200

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import FancyBorder from '$lib/components/generic/FancyBorder.svelte';
 	import { page } from '$app/stores';
 	import { invalidateAll } from '$app/navigation';
 	import { redirect } from '@sveltejs/kit';
@@ -18,8 +17,6 @@
 	<h1 class="text-xl mt-2">I think you got lost :c</h1>
 
 	<span class="mt-2">The page you were looking for, doesn't exist</span>
-
-	<FancyBorder />
 
 	<a href="/" class="btn-medium mt-2">Take me back</a>
 </div>
